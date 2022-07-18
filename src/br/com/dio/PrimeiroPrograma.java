d5961af38dc8158b140a167e1315279683f9084f
verediana <verediana.csilva@outlook.com>
2022-07-18 00:10:15 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+
+	public static void main(String[] args) {
+	
+		Gato Gato = new Gato();		
+		System.out.println("Hello world");
+	}
+
+	}
+
